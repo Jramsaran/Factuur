a5af0fa960ea63c73f5452e7acf5561e83b75828
Jramsaran <jeremy.ramsaran@hotmail.com>
2022-03-08 10:14:38 +0100

    Commit on main

--- apps/pdf_reading.py
+++ apps/pdf_reading.py
@@ -3,19 +3,13 @@ import pdfplumber
 import PyPDF2
 import os
 import random
+import re
+
 
 class Read_PDF:
 
     def __init__(self, pdf_file: str) -> None:
-        
-        self.pdf_file = pdf_file
 
-
-class Rotate_PDF(Read_PDF):
-
-    def __init__(self, pdf_file: str) -> None:
-
-        super().__init__(pdf_file)
         self.pdf_file = pdf_file
 
     def create_temp_file(self, pdf_file: str, file_num: int = 0) -> str:
@@ -45,13 +39,6 @@ class Rotate_PDF(Read_PDF):
 
         pdf_file = self.pdf_file
 
-        try:
-            int_rotation = int(rotation_degree)
-
-        except ValueError:
-
-            f"ValueError: {rotation_degree} is not a number."
-
         temp_file_path = self.create_temp_file(pdf_file)
         
         with open(pdf_file, 'rb') as pdf_in:
@@ -60,7 +47,7 @@ class Rotate_PDF(Read_PDF):
 
             for pagenum in range(pdf_reader.numPages):
                 page = pdf_reader.getPage(pagenum)
-                page.rotateClockwise(int_rotation)
+                page.rotateClockwise(rotation_degree)
                 pdf_writer.addPage(page)
 
             with open(temp_file_path, 'wb') as pdf_out:
@@ -70,10 +57,51 @@ class Rotate_PDF(Read_PDF):
         print(f"Temp file {temp_file_path} created")
         return temp_file_path
 
+    def read_pdf(self, pdf_file: str) -> str:
+
+        with pdfplumber.open(pdf_file) as pdf:
+
+            compiled_text = ""
+
+            for n in range(len(pdf.pages)):
+
+                page = pdf.pages[n]
+                text = page.extract_text()
+                compiled_text += text
+
+            return compiled_text
+
+    def cidToChar(self, cidx):
+        return chr(int(re.findall(r'\(cid\:(\d+)\)', cidx)[0]) + 29)
+    
+    def decode_pdf(self, extracted_text: str) -> str:
+
+        text = {}
+        compiled_text = ""
+
+        for index, line in enumerate(extracted_text.split('\n')):
+            text[index] = []
+            if line != '':
+                regex_result = re.findall(r'\(cid\:\d+\)', line)
+                if len(regex_result) > 0:
+                    for cid in regex_result:
+                        line = line.replace(cid, self.cidToChar(cid))
+                        text[index].append(line)
+                compiled_text += repr(line).strip("'").replace('Ù', '€')
+                compiled_text += '\n' 
+        
+        compiled_text = compiled_text.rstrip() # remove trailing newline
+
+        return compiled_text
 
 if __name__ == '__main__':
 
-    XD = Rotate_PDF(r'C:\\Users\\Jeremy\\Downloads\\CharterKosten-2366.pdf')
+    pdf = Read_PDF(r'C:\\Users\\Jeremy\\Downloads\\CharterKosten-2366.pdf')
+    temp_file_path = pdf.rotator(270)
 
-    temp_file_path = XD.rotator(180)
-    XD.delete_temp_file(temp_file_path)
+    extracted_text = pdf.read_pdf(temp_file_path)
+    decoded_text = pdf.decode_pdf(extracted_text)
+
+    pdf.delete_temp_file(temp_file_path)
+
+    print(decoded_text)
